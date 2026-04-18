559760cf4271500ca20477cb95327886917c0c09
Geof-0 <geckoeater61@gmail.com>
2026-04-18 11:19:47 -0500

    remove .exe
    no no no you shalt not do that

--- main.cpp
+++ main.cpp
@@ -1,84 +1,143 @@
-// glad / glfw library include statements
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-// other libs
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #include <iostream>
-#include <future>
 
+const char* vs = R"(
+#version 330 core
+layout(location = 0) in vec2 aPos;
+layout(location = 1) in vec2 aUV;
+out vec2 uv;
+void main() {
+    uv = aUV;
+    gl_Position = vec4(aPos, 0.0, 1.0);
+}
+)";
 
+const char* fs = R"(
+#version 330 core
+out vec4 color;
+in vec2 uv;
+uniform sampler2D tex;
+void main() {
+    color = texture(tex, uv);
+}
+)";
 
-// functions and stuff
+GLuint compile(GLenum type, const char* src) {
+    GLuint s = glCreateShader(type);
+    glShaderSource(s, 1, &src, NULL);
+    glCompileShader(s);
+    return s;
+}
 
+GLuint makeProgram() {
+    GLuint v = compile(GL_VERTEX_SHADER, vs);
+    GLuint f = compile(GL_FRAGMENT_SHADER, fs);
 
-int main() {
+    GLuint p = glCreateProgram();
+    glAttachShader(p, v);
+    glAttachShader(p, f);
+    glLinkProgram(p);
 
+    glDeleteShader(v);
+    glDeleteShader(f);
 
+    return p;
+}
 
+struct Sprite {
+    GLuint vao, vbo, texture;
+    GLuint program;
 
+    void init(const char* file) {
+        float verts[] = {
+            -0.5f,-0.5f, 0,0,
+             0.5f,-0.5f, 1,0,
+             0.5f, 0.5f, 1,1,
 
+             0.5f, 0.5f, 1,1,
+            -0.5f, 0.5f, 0,1,
+            -0.5f,-0.5f, 0,0
+        };
 
+        glGenVertexArrays(1, &vao);
+        glGenBuffers(1, &vbo);
 
+        glBindVertexArray(vao);
 
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
 
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+        glEnableVertexAttribArray(1);
 
+        int w, h, c;
+        stbi_set_flip_vertically_on_load(true);
+        unsigned char* data = stbi_load(file, &w, &h, &c, 0);
 
-    // start rendering (should be at end)
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
 
-    // init glfw
-    if (!glfwInit()) {
-        std::cout << "Failed to init GLFW\n";
-        return -1;
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        GLenum format = (c == 4) ? GL_RGBA : GL_RGB;
+
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, format, w, h, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        stbi_image_free(data);
+
+        program = makeProgram();
+
+        glUseProgram(program);
+        glUniform1i(glGetUniformLocation(program, "tex"), 0);
     }
 
+    void draw() {
+        glUseProgram(program);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+};
+
+int main() {
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    // create window
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
-    if (!window) {
-        std::cout << "Failed to create window\n";
-        glfwTerminate();
-        return -1;
-    }
-
+    GLFWwindow* window = glfwCreateWindow(800, 600, "sprite", NULL, NULL);
     glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    // load openGL glad
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD\n";
-        return -1;
-    }
+    glViewport(0, 0, 800, 600);
 
-    // image test 
+    Sprite sprite;
+    sprite.init("test.png");
 
-    stbi_set_flip_vertically_on_load(true);
-
-    int w, h, ch;
-    unsigned char* test = stbi_load("test.png", &w, &h, &ch, 4);
-
-    if (!test) {
-        std::cout << "FAILED TO LOAD IMAGE\n";
-    }
-
-    // set viewport
-    glViewport(0, 0, 1200, 1200);
 
     // render loop
     while (!glfwWindowShouldClose(window)) {
-        // backgound bluu
         glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        sprite.draw();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
-    return 0;
 }
