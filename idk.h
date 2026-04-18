df5b5073f98089cf65233c9471c31c5ac1e01102
Geof-0 <geckoeater61@gmail.com>
2026-04-17 19:26:37 -0500

    agnpoaneopiuaboiugb
    afbofaiubfoiybeaoiufboeiuf

---
+++ idk.h
@@ -0,0 +1,7 @@
+#ifndef STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+
+// include stb image so sprites can be rendered (FOR THE LOVE OF GOD DO NOT MISTAKENLY REMOVE THIS FILE)
+#include "stb_image.h"
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
+// glad / glfw library include statements
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+// other libs
+
 #include <iostream>
+#include <future>
+
+
+
+// functions and stuff
+
 
 int main() {
+
+
+
+
+
+
+
+
+
+
+
+
+    // start rendering (should be at end)
+
     // init glfw
     if (!glfwInit()) {
         std::cout << "Failed to init GLFW\n";
@@ -25,18 +49,29 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
-    // load OpenGL glad
+    // load openGL glad
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD\n";
         return -1;
     }
 
-    // set viewport
-    glViewport(0, 0, 800, 600);
+    // image test 
 
-    // Render loop
+    stbi_set_flip_vertically_on_load(true);
+
+    int w, h, ch;
+    unsigned char* test = stbi_load("test.png", &w, &h, &ch, 4);
+
+    if (!test) {
+        std::cout << "FAILED TO LOAD IMAGE\n";
+    }
+
+    // set viewport
+    glViewport(0, 0, 1200, 1200);
+
+    // render loop
     while (!glfwWindowShouldClose(window)) {
-        // backgound geen
+        // backgound bluu
         glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
