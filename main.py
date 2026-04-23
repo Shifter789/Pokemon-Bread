65b8faa98499c30471e976c564c827ff3ebdd19f
Percy <percygandhi211@gmail.com>
2026-04-22 23:50:50 -0500

    Added test character for movement logic.

--- main.py
+++ main.py
@@ -16,17 +16,44 @@ fps = 75
 screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Title_holder")
 
+#movement
+test_x = 50
+test_y = 50
+width = 40
+height = 60
+vel = 5
 
 #main_loop
 
 running = True
     
 while running:
+    pygame.time.delay(10)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
 
+    #keyboard presses
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_w]:
+        test_y -= vel
+
+    if keys[pygame.K_a]:
+        test_x -= vel
+
+    if keys[pygame.K_s]:
+        test_y += vel
+
+    if keys[pygame.K_d]:
+        test_x += vel
+
+
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, (255, 0, 0), (test_x, test_y, width, height))
     pygame.display.update()
     clock.tick(fps)
 
